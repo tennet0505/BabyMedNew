4b4f07ad7947d5cbc6625092e35c8d548e4ef98b
Tennet <tennet0505@gmail.com>
2018-07-03 10:21:29 +0600

    Initial Commit

---
+++ BabyMedNew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BabyMedNew
+//
+//  Created by Oleg Ten on 7/3/18.
+//  Copyright © 2018 Oleg Ten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
